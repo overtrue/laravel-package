6e9e9dbcba419d462d8735470eaba3577eabb3d0
overtrue <anzhengchao@gmail.com>
2020-04-21 11:22:15 +0800

    init commit.

---
+++ tests/TestCase.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the overtrue/laravel-follow.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Tests;
+
+use Overtrue\LaravelPackage\PackageServiceProvider;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Load package service provider.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [PackageServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->loadMigrationsFrom(__DIR__ . '/migrations');
+        $this->loadMigrationsFrom(dirname(__DIR__) . '/migrations');
+    }
+}
